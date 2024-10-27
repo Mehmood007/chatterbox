36012d3cad81dfb15537138b8c6d6f7b36d952fa
Mehmood Hassan Shah <mehmoodhassan595@yahoo.com>
2024-10-27 12:52:06 -0400

    basic tables structure defined

--- backend/djchat/settings.py
+++ backend/djchat/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom applications
+    'account',
+    'server',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'account.Account'
